bcmIntc/SOS

commit d759cb20c7e1bff03712f3fd2965ed96bddde383

Reasonable defaults
Signed-off-by: Mark F. Brown <mark.f.brown@intel.com>

configure.ac
@@ -13,7 +13,7 @@
 
 dnl Init Autoconf/Automake/Libtool
 
-AC_INIT([Sandia OpenSHMEM], [1.5.3], [https://github.com/Sandia-OpenSHMEM/SOS/issues], [sandia-openshmem], [https://github.com/Sandia-OpenSHMEM/SOS])
+AC_INIT([Sandia OpenSHMEM], [1.6.0], [https://github.com/Sandia-OpenSHMEM/SOS/issues], [sandia-openshmem], [https://github.com/Sandia-OpenSHMEM/SOS])
 AC_PREREQ([2.60])
 AC_CONFIG_AUX_DIR([config])
 AC_CONFIG_MACRO_DIR([config])
@@ -43,8 +43,10 @@ OPAL_CONFIGURE_SETUP
 OPAL_BASIC_SETUP
 
 OPAL_CHECK_ATTRIBUTES
+
+#disable deprecation warnings
 AS_IF([test "$opal_cv___attribute__deprecated" = 1],
-      [SHMEM_AC_HAVE_ATTRIBUTE_DEPRECATED=1],
+      [SHMEM_AC_HAVE_ATTRIBUTE_DEPRECATED=0],
       [SHMEM_AC_HAVE_ATTRIBUTE_DEPRECATED=0])
 AC_SUBST(SHMEM_AC_HAVE_ATTRIBUTE_DEPRECATED)
 
@@ -95,7 +97,8 @@ AS_IF([test "$enable_completion_polling" = "yes" -o "$enable_manual_progress" = 
       [AC_DEFINE([DEFAULT_POLL_LIMIT], [-1], [Poll limit for local/remote completions])],
       [AC_DEFINE([DEFAULT_POLL_LIMIT], [0], [Poll limit for local/remote completions])])
 
-AS_IF([test "$enable_manual_progress" = "yes"],
+AS_IF([test "$enable_manual_progress" = "no"],
+      [AC_DEFINE([ENABLE_MANUAL_PROGRESS], [0], [Enable manual progress])],
       [AC_DEFINE([ENABLE_MANUAL_PROGRESS], [1], [Enable manual progress])])
 
 AC_ARG_ENABLE([total-data-ordering],
@@ -212,9 +215,9 @@ AC_ARG_ENABLE([c11-checks],
                     [Disable build-time type checks for C11 bindings (default: enabled)])])
 
 AC_ARG_ENABLE([libtool-wrapper],
-  [AS_HELP_STRING([--disable-libtool-wrapper],
-    [Disable use of libtool executable wrappers for tests (default: enabled)])])
-AS_IF([test "$enable_libtool_wrapper" = "no"], [LIBTOOL_WRAPPER_LDFLAGS="-no-install"], [LIBTOOL_WRAPPER_LDFLAGS=])
+  [AS_HELP_STRING([--enable-libtool-wrapper],
+    [Enable use of libtool executable wrappers for tests (default: disabled)])])
+AS_IF([test "$enable_libtool_wrapper" = "yes"], [LIBTOOL_WRAPPER_LDFLAGS=], [LIBTOOL_WRAPPER_LDFLAGS="-no-install"])
 AC_SUBST([LIBTOOL_WRAPPER_LDFLAGS])
 
 AC_ARG_ENABLE([rpath],
@@ -257,15 +260,17 @@ AS_CASE([$enable_ofi_mr],
         [AC_MSG_ERROR([Invalid OFI memory registration mode: $enable_ofi_mr])])
 
 AC_ARG_ENABLE([mr-endpoint],
-    [AS_HELP_STRING([--enable-mr-endpoint],
-                    [Use FI_MR_ENDPOINT to enable cxi provider. (default: disabled)])])
-AS_IF([test "$enable_mr_endpoint" = "yes"],
+    [AS_HELP_STRING([--disable-mr-endpoint],
+                    [Use FI_MR_ENDPOINT to enable cxi provider. (default: enabled)])])
+AS_IF([test "$enable_mr_endpoint" = "no"],
+      [AC_DEFINE([ENABLE_MR_ENDPOINT], [0], [If defined, the OFI transport will use FI_MR_ENDPOINT])],
       [AC_DEFINE([ENABLE_MR_ENDPOINT], [1], [If defined, the OFI transport will use FI_MR_ENDPOINT])])
 
 AC_ARG_ENABLE([ofi-manual-progress],
-    [AS_HELP_STRING([--enable-ofi-manual-progress],
-                    [Use FI_MANUAL_PROGRESS for data progress control mode. (default: disabled)])])
-AS_IF([test "$enable_ofi_manual_progress" = "yes"],
+    [AS_HELP_STRING([--disable-ofi-manual-progress],
+                    [Use FI_MANUAL_PROGRESS for data progress control mode. (default: enabled)])])
+AS_IF([test "$enable_ofi_manual_progress" = "no"],
+      [AC_DEFINE([ENABLE_FI_MANUAL_PROGRESS], [0], [If defined, the OFI will use FI_MANUAL_PROGRESS as data progress mode. This is currently required for cxi provider.])],
       [AC_DEFINE([ENABLE_FI_MANUAL_PROGRESS], [1], [If defined, the OFI will use FI_MANUAL_PROGRESS as data progress mode. This is currently required for cxi provider.])])
 
 AC_ARG_ENABLE([max-teams],
@@ -289,11 +294,11 @@ AS_IF([test "$enable_av_map" = "yes"],
       [AC_DEFINE([USE_AV_MAP], [1], [If defined, the OFI transport will use FI_AV_MAP.  Otherwise, FI_AV_TABLE is used.])])
 
 AC_ARG_ENABLE([bounce-buffers],
-    [AS_HELP_STRING([--disable-bounce-buffers],
-                    [Disable bounce buffers by default (default: enabled)])])
-AS_IF([test "$enable_bounce_buffers" = "no"],
-      [AC_DEFINE([DEFAULT_BOUNCE_SIZE], [0], [Default bounce buffer threshold])],
-      [AC_DEFINE([DEFAULT_BOUNCE_SIZE], [2048], [Default bounce buffer threshold])])
+    [AS_HELP_STRING([--enable-bounce-buffers],
+                    [Enable bounce buffers by default (default: disabled)])])
+AS_IF([test "$enable_bounce_buffers" = "yes"],
+      [AC_DEFINE([DEFAULT_BOUNCE_SIZE], [2048], [Default bounce buffer threshold])],
+      [AC_DEFINE([DEFAULT_BOUNCE_SIZE], [0], [Default bounce buffer threshold])])
 
 AC_ARG_ENABLE([ofi-inject],
     [AS_HELP_STRING([--disable-ofi-inject],
@@ -328,11 +333,11 @@ AS_IF([test "$enable_shr_atomics" = "yes"],
       [AC_DEFINE([USE_SHR_ATOMICS], [1], [If defined, the shared memory layer will perform processor atomics.])])
 
 AC_ARG_ENABLE([hierarchical-barrier],
-    [AS_HELP_STRING([--enable-hierarchical-barrier],
+    [AS_HELP_STRING([--disable-hierarchical-barrier],
                     [Enable the hierarchical barrier: intranode phase uses CPU atomics,
                      internode phase uses NIC puts. Requires an on-node transport (XPMEM
                      or CMA) and a network transport (OFI, Portals4, or UCX).
-                     (default: disabled)])])
+                     (default: enabled)])])
 
 AC_ARG_ENABLE([manpages],
     [AS_HELP_STRING([--enable-manpages],
@@ -561,18 +566,23 @@ dnl Hierarchical barrier: only when explicitly requested.  Validate that an
 dnl on-node transport (XPMEM or CMA) and a network transport are both present;
 dnl error if dependencies are missing.
 transport_hierarchical_barrier="no"
-if test "$enable_hierarchical_barrier" = "yes"; then
+if test "$enable_hierarchical_barrier" = "no"; then
+    AC_MSG_WARN([Hierachical Barrier disabled])
+else
     dnl Check on-node transport dependency
     if test "$transport_xpmem" != "yes" -a "$transport_cma" != "yes"; then
-        AC_MSG_ERROR([--enable-hierarchical-barrier requires an on-node transport; configure with --with-xpmem or --with-cma])
+        AC_MSG_WARN([--enable-hierarchical-barrier requires an on-node transport; configure with --with-xpmem or --with-cma])
     dnl Check network transport dependency
     elif test "$transport_ofi" != "yes" -a "$transport_portals4" != "yes" -a "$transport_ucx" != "yes"; then
-        AC_MSG_ERROR([--enable-hierarchical-barrier requires a network transport; configure with --with-ofi, --with-portals4, or --with-ucx])
+        AC_MSG_WARN([--enable-hierarchical-barrier requires a network transport; configure with --with-ofi, --with-portals4, or --with-ucx])
     else
         transport_hierarchical_barrier="yes"
     fi
 fi
-if test "$transport_hierarchical_barrier" = "yes"; then
+if test "$transport_hierarchical_barrier" = "no"; then
+    AC_DEFINE([USE_HIERARCHICAL_BARRIER], [0],
+              [If defined, enables the hierarchical barrier: intranode CPU atomics + internode NIC puts.])
+else
     AC_DEFINE([USE_HIERARCHICAL_BARRIER], [1],
               [If defined, enables the hierarchical barrier: intranode CPU atomics + internode NIC puts.])
     dnl The hierarchical barrier uses shared-memory puts/gets (XPMEM/CMA) for

src/collectives.c
@@ -565,6 +565,7 @@ shmem_internal_build_root_active_set(int PE_start, int PE_stride, int PE_size,
     }
 }
 
+#if 0
 /* CPU atomic load of the long at `target` via local mapped pointer. */
 static inline long
 shmem_internal_cpu_atomic_load_long(long *target, int noderank)
@@ -575,6 +576,7 @@ shmem_internal_cpu_atomic_load_long(long *target, int noderank)
     __atomic_load((long *)remote_ptr, &val, __ATOMIC_ACQUIRE);
     return val;
 }
+#endif
 
 /* CPU atomic store of `val` to the long at `target` via local mapped pointer. */
 static inline void
@@ -619,14 +621,9 @@ shmem_internal_sync_hierarchical(int PE_start, int PE_stride, int PE_size,
                   ? (my_local_idx - root_local_idx + local_count) % local_count
                   : -1;
 
-    int  tree_parent_shr = -1;
     int  tree_nchildren  = 0;
     int *tree_child_shr  = alloca(sizeof(int) * tree_radix);
     if (my_vidx >= 0) {
-        if (my_vidx > 0) {
-            tree_parent_shr = shmem_runtime_get_node_rank(
-                local_pes[((my_vidx - 1) / tree_radix + root_local_idx) % local_count]);
-        }
         for (int j = 1; j <= tree_radix; j++) {
             int cv = my_vidx * tree_radix + j;
             if (cv < local_count) {
@@ -648,7 +645,6 @@ shmem_internal_sync_hierarchical(int PE_start, int PE_stride, int PE_size,
     void *my_up_raw, *my_down_raw;
     shmem_shr_transport_ptr(&up_pSync[my_shr_rank * HIER_SLOT_STRIDE],   my_shr_rank, &my_up_raw);
     shmem_shr_transport_ptr(&down_pSync[my_shr_rank * HIER_SLOT_STRIDE], my_shr_rank, &my_down_raw);
-    volatile long *my_up_slot   = (volatile long *)my_up_raw;
     volatile long *my_down_slot = (volatile long *)my_down_raw;
 
     /* ---- Degenerate case: all active PEs on one node ---- */

src/init.c
@@ -251,23 +251,22 @@ shmem_internal_heap_preinit(int tl_requested, int *tl_provided)
             shmem_internal_params.DEBUG)
         {
             printf(PACKAGE_STRING "\n");
-        }
-
-        if (shmem_internal_params.INFO) {
-            shmem_internal_print_env();
-            printf("\n");
+            printf("Build information:\n");
+#ifdef SOS_GIT_VERSION
+            printf("%-23s %s\n", "  Git Version", SOS_GIT_VERSION);
+#else
+	    printf("%-23s %d\.%d\n", "Build Version", SHMEM_MAJOR_VERSION, SHMEM_MINOR_VERSION);
+#endif
         }
 
         if (shmem_internal_params.DEBUG) {
+            shmem_internal_print_env();
+            printf("\n");
+
             char *wrapped_configure_args = shmem_util_wrap(SOS_CONFIGURE_ARGS, 60,
                                                            "                        ");
             char *wrapped_build_cflags   = shmem_util_wrap(SOS_BUILD_CFLAGS, 60,
                                                            "                        ");
-
-            printf("Build information:\n");
-#ifdef SOS_GIT_VERSION
-            printf("%-23s %s\n", "  Git Version", SOS_GIT_VERSION);
-#endif
             printf("%-23s %s\n", "  Configure Args", wrapped_configure_args);
             printf("%-23s %s\n", "  Build Date", SOS_BUILD_DATE);
             printf("%-23s %s\n", "  Build CC", SOS_BUILD_CC);

src/shmem_env_defs.h
@@ -22,7 +22,7 @@
  * Kinds: long, size, bool, string
  * Categories: openshmem, other, collectives, intranode, transport
  */
-SHMEM_INTERNAL_ENV_DEF(INFO, bool, false, SHMEM_INTERNAL_ENV_CAT_OPENSHMEM,
+SHMEM_INTERNAL_ENV_DEF(INFO, bool, true, SHMEM_INTERNAL_ENV_CAT_OPENSHMEM,
                        "Print library information message at startup")
 SHMEM_INTERNAL_ENV_DEF(VERSION, bool, false, SHMEM_INTERNAL_ENV_CAT_OPENSHMEM,
                        "Print library version at startup")
@@ -32,7 +32,7 @@ SHMEM_INTERNAL_ENV_DEF(SYMMETRIC_SIZE, size, 512*1024*1024, SHMEM_INTERNAL_ENV_C
                        "Symmetric heap size")
 
 #ifdef __linux__
-SHMEM_INTERNAL_ENV_DEF(SYMMETRIC_HEAP_USE_HUGE_PAGES, bool, false, SHMEM_INTERNAL_ENV_CAT_OTHER,
+SHMEM_INTERNAL_ENV_DEF(SYMMETRIC_HEAP_USE_HUGE_PAGES, bool, true, SHMEM_INTERNAL_ENV_CAT_OTHER,
                        "Use Linux huge pages for symmetric heap")
 SHMEM_INTERNAL_ENV_DEF(SYMMETRIC_HEAP_PAGE_SIZE, size, 2*1024*1024, SHMEM_INTERNAL_ENV_CAT_OTHER,
                        "Page size to use for huge pages")

commit 2088f37790d872290b928a4f3e2ddac80fdfa8f2

configure,collectives: fix #ifdef guards broken by d759cb20 =0 defines
d759cb20 ("Reasonable defaults") defined USE_HIERARCHICAL_BARRIER,
ENABLE_MR_ENDPOINT, and ENABLE_FI_MANUAL_PROGRESS to 0 when their
respective disable flags were passed. All usage sites test these with
#ifdef, so the symbol being defined to 0 still evaluated true, silently
enabling features that were explicitly disabled. On CMA builds this
caused XPMEM-dependent barrier code to compile in, triggering a runtime
abort in shmem_shr_transport_ptr.

Remove the =0 branches for all three symbols; disabled builds leave
them undefined. Also fix a dangling case HIERARCHICAL outside its
#ifdef block in shmem_collectives.h.

configure.ac
@@ -262,15 +262,13 @@ AS_CASE([$enable_ofi_mr],
 AC_ARG_ENABLE([mr-endpoint],
     [AS_HELP_STRING([--disable-mr-endpoint],
                     [Use FI_MR_ENDPOINT to enable cxi provider. (default: enabled)])])
-AS_IF([test "$enable_mr_endpoint" = "no"],
-      [AC_DEFINE([ENABLE_MR_ENDPOINT], [0], [If defined, the OFI transport will use FI_MR_ENDPOINT])],
+AS_IF([test "$enable_mr_endpoint" != "no"],
       [AC_DEFINE([ENABLE_MR_ENDPOINT], [1], [If defined, the OFI transport will use FI_MR_ENDPOINT])])
 
 AC_ARG_ENABLE([ofi-manual-progress],
     [AS_HELP_STRING([--disable-ofi-manual-progress],
                     [Use FI_MANUAL_PROGRESS for data progress control mode. (default: enabled)])])
-AS_IF([test "$enable_ofi_manual_progress" = "no"],
-      [AC_DEFINE([ENABLE_FI_MANUAL_PROGRESS], [0], [If defined, the OFI will use FI_MANUAL_PROGRESS as data progress mode. This is currently required for cxi provider.])],
+AS_IF([test "$enable_ofi_manual_progress" != "no"],
       [AC_DEFINE([ENABLE_FI_MANUAL_PROGRESS], [1], [If defined, the OFI will use FI_MANUAL_PROGRESS as data progress mode. This is currently required for cxi provider.])])
 
 AC_ARG_ENABLE([max-teams],
@@ -589,10 +587,7 @@ else
         transport_hierarchical_barrier="yes"
     fi
 fi
-if test "$transport_hierarchical_barrier" = "no"; then
-    AC_DEFINE([USE_HIERARCHICAL_BARRIER], [0],
-              [If defined, enables the hierarchical barrier: intranode CPU atomics + internode NIC puts.])
-    else
+if test "$transport_hierarchical_barrier" = "yes"; then
     AC_DEFINE([USE_HIERARCHICAL_BARRIER], [1],
               [If defined, enables the hierarchical barrier: intranode CPU atomics + internode NIC puts.])
     dnl The hierarchical barrier uses XPMEM-mapped pointers for intranode data

src/shmem_collectives.h
@@ -101,9 +101,13 @@ shmem_internal_sync(int PE_start, int PE_stride, int PE_size, long *pSync)
         case DISSEM: algo = "DISSEM"; break;
         case RING: algo = "RING"; break;
         case RECDBL: algo = "RECDBL"; break;
+        case HIERARCHICAL:
 #ifdef USE_HIERARCHICAL_BARRIER
-        case HIERARCHICAL: algo = "HIERARCHICAL"; break;
+            algo = "HIERARCHICAL";
+#else
+            algo = (PE_size < shmem_internal_params.COLL_CROSSOVER) ? "AUTO(LINEAR)" : "AUTO(TREE)";
 #endif
+            break;
         }
         DEBUG_MSG("Barrier algorithm: %s (type=%d, PE_size=%d, CROSSOVER=%ld, hier_auto=%d)\n",
                   algo, shmem_internal_barrier_type, PE_size,
@@ -149,14 +153,15 @@ shmem_internal_sync(int PE_start, int PE_stride, int PE_size, long *pSync)
     case DISSEM:
         shmem_internal_sync_dissem(PE_start, PE_stride, PE_size, pSync);
         break;
-#ifdef USE_HIERARCHICAL_BARRIER
     case HIERARCHICAL:
+#ifdef USE_HIERARCHICAL_BARRIER
         shmem_internal_sync_hierarchical(PE_start, PE_stride, PE_size,
                                          pSync,
                                          shmem_internal_hierarchical_local_psync,
                                          NULL, NULL);
         break;
 #endif
+        /* fall through to tree/linear if hierarchical barrier not compiled in */
     default:
         RAISE_ERROR_MSG("Illegal barrier/sync type (%d)\n",
                         shmem_internal_barrier_type);